7402ec16475afe52bd41b15d01acc457d9f848be
Jakub Suder <jakub.suder@gmail.com>
2015-03-08 21:33:30 +0100

    fix

--- sparkler.rb
+++ sparkler.rb
@@ -13,12 +13,10 @@ class Sparkler
       feed = @feeds[feed_name]
 
       if feed
-        now = Time.now
-        year, month = now.year, now.month
-
         if (userAgent = env["HTTP_USER_AGENT"]) && userAgent =~ %r(Sparkle/)
-          save_params(request.params)
-          save_param('appVersionShort', userAgent.split(' ').first.split('/').last)
+          now = Time.now
+          save_params(feed, now, request.params)
+          save_param(feed, now, 'appVersionShort', userAgent.split(' ').first.split('/').last)
         end
 
         feed.contents ? success(feed.contents) : not_found
@@ -38,22 +36,22 @@ class Sparkler
     Rack::Response.new("Not found :-(\n", 404).to_a
   end
 
-  def save_params(params)
+  def save_params(feed, time, params)
     params = params.clone
     params.delete('appName')
 
     params.each do |property_name, value_name|
-      save_param(property_name, value_name)
+      save_param(feed, time, property_name, value_name)
     end
   end
 
-  def save_param(property_name, value_name)
+  def save_param(feed, time, property_name, value_name)
     property = @properties[property_name] ||= Property.find_or_create_by(name: property_name)
     value = property.values.detect { |v| v.name == value_name } || property.values.create(name: value_name)
 
     statistic = Statistic.find_or_create_by(
-      year: year,
-      month: month,
+      year: time.year,
+      month: time.month,
       feed: feed,
       property: property,
       value: value
