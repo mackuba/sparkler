b16ef514aa12cfb45f889d76629352fb4ff03bee
Jakub Suder <jakub.suder@gmail.com>
2015-03-08 21:22:40 +0100

    ignore requests not from Sparkle
    there are apps that automatically check for updates of all the apps on
    your system, but they might not necessarily respect our interval of
    submitting profile data (= once a week, even if update checks happen
    every day), so it’s best to ignore those

--- sparkler.rb
+++ sparkler.rb
@@ -16,23 +16,8 @@ class Sparkler
         now = Time.now
         year, month = now.year, now.month
 
-        params = request.params.clone
-        params['userAgent'] = env["HTTP_USER_AGENT"].to_s.split(/\//).first
-        params.delete('appName')
-
-        params.each do |property_name, value_name|
-          property = @properties[property_name] ||= Property.find_or_create_by(name: property_name)
-          value = property.values.detect { |v| v.name == value_name } || property.values.create(name: value_name)
-
-          statistic = Statistic.find_or_create_by(
-            year: year,
-            month: month,
-            feed: feed,
-            property: property,
-            value: value
-          )
-
-          Statistic.update_counters(statistic.id, counter: 1)
+        if env["HTTP_USER_AGENT"].to_s =~ %r(Sparkle/)
+          save_params(request.params)
         end
 
         feed.contents ? success(feed.contents) : not_found
@@ -50,5 +35,25 @@ class Sparkler
 
   def not_found
     Rack::Response.new("Not found :-(\n", 404).to_a
+  end
+
+  def save_params(params)
+    params = params.clone
+    params.delete('appName')
+
+    params.each do |property_name, value_name|
+      property = @properties[property_name] ||= Property.find_or_create_by(name: property_name)
+      value = property.values.detect { |v| v.name == value_name } || property.values.create(name: value_name)
+
+      statistic = Statistic.find_or_create_by(
+        year: year,
+        month: month,
+        feed: feed,
+        property: property,
+        value: value
+      )
+
+      Statistic.update_counters(statistic.id, counter: 1)
+    end
   end
 end
