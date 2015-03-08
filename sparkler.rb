710405b3cc27f519bd60aeb5e283ac3f0f6345e3
Jakub Suder <jakub.suder@gmail.com>
2015-03-08 21:29:04 +0100

    save also app version from the user agent string
    appVersion is set to the build number, which might be different than the
    displayed version string

--- sparkler.rb
+++ sparkler.rb
@@ -16,8 +16,9 @@ class Sparkler
         now = Time.now
         year, month = now.year, now.month
 
-        if env["HTTP_USER_AGENT"].to_s =~ %r(Sparkle/)
+        if (userAgent = env["HTTP_USER_AGENT"]) && userAgent =~ %r(Sparkle/)
           save_params(request.params)
+          save_param('appVersionShort', userAgent.split(' ').first.split('/').last)
         end
 
         feed.contents ? success(feed.contents) : not_found
@@ -42,18 +43,22 @@ class Sparkler
     params.delete('appName')
 
     params.each do |property_name, value_name|
-      property = @properties[property_name] ||= Property.find_or_create_by(name: property_name)
-      value = property.values.detect { |v| v.name == value_name } || property.values.create(name: value_name)
-
-      statistic = Statistic.find_or_create_by(
-        year: year,
-        month: month,
-        feed: feed,
-        property: property,
-        value: value
-      )
-
-      Statistic.update_counters(statistic.id, counter: 1)
+      save_param(property_name, value_name)
     end
+  end
+
+  def save_param(property_name, value_name)
+    property = @properties[property_name] ||= Property.find_or_create_by(name: property_name)
+    value = property.values.detect { |v| v.name == value_name } || property.values.create(name: value_name)
+
+    statistic = Statistic.find_or_create_by(
+      year: year,
+      month: month,
+      feed: feed,
+      property: property,
+      value: value
+    )
+
+    Statistic.update_counters(statistic.id, counter: 1)
   end
 end
