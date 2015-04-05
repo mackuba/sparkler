9d9a655bbacf04aee542a84e2ebad779e89687e5
Jakub Suder <jakub.suder@gmail.com>
2015-04-05 21:17:59 +0200

    finished feeds#show + removed old sparkler.rb

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,6 +4,15 @@ class Feed < ActiveRecord::Base
   has_many :statistics
   validates_presence_of :title, :name, :url
 
+  def self.all_feeds
+    @@feeds ||= Feed.all.to_a
+  end
+
+  def self.get_by_name(name)
+    feed = all_feeds.detect { |f| f.name == name }
+    feed or raise ActiveRecord::RecordNotFound.new("Couldn't find Feed with name='#{name}'")
+  end
+
   def title=(title)
     write_attribute :name, title.downcase.gsub(/\W+/, '_') if title
     write_attribute :title, title
@@ -18,5 +27,28 @@ class Feed < ActiveRecord::Base
     @contents
   rescue OpenURI::HTTPError => error
     puts "Couldn't download feed from #{url}: #{error}"
+  end
+
+  def save_params(timestamp, params)
+    params = params.clone
+    params.delete('appName')
+
+    params.each do |property_name, value_name|
+      save_param(timestamp, property_name, value_name)
+    end
+  end
+
+  def save_param(timestamp, property_name, value_name)
+    property = Property.find_or_create_by(name: property_name)
+    value = property.values.find_or_create_by(name: value_name)
+
+    statistic = self.statistics.find_or_create_by(
+      year: timestamp.year,
+      month: timestamp.month,
+      property: property,
+      value: value
+    )
+
+    Statistic.update_counters(statistic.id, counter: 1)
   end
 end
