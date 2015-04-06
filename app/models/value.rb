99e0509df3e9706cde2a41028adc0464496699a9
Jakub Suder <jakub.suder@gmail.com>
2015-04-06 22:03:50 +0200

    use readable titles for property values when possible

--- app/models/value.rb
+++ app/models/value.rb
@@ -1,4 +1,15 @@
 class Value < ActiveRecord::Base
   belongs_to :property
   validates_presence_of :name
+
+  HUMAN_READABLE_NAMES = {
+    'cputype' => {
+      '7' => 'Intel',
+      '18' => 'PowerPC'
+    },
+    'cpu64bit' => {
+      '0' => '32-bit',
+      '1' => '64-bit'
+    }
+  }
 end
