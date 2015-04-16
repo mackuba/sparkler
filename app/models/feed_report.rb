a6e36dd0d3187855a47b8e620a11c5f4e420316d
Kuba Suder <jakub.suder@gmail.com>
2015-04-17 01:23:42 +0200

    added option names for some newer Mac models

--- app/models/feed_report.rb
+++ app/models/feed_report.rb
@@ -24,6 +24,36 @@ class FeedReport
       :field => 'model',
       :threshold => 7.5,
       :show_other => false,
+      :options => {
+        'iMac12,1' => 'iMac 21" (2011)',
+        'iMac12,2' => 'iMac 27" (2011)',
+        'iMac13,1' => 'iMac 21" (2012)',
+        'iMac13,2' => 'iMac 27" (2012)',
+        'iMac14,1' => 'iMac 21" (2013)',
+        'iMac14,2' => 'iMac 27" (2013)',
+        'iMac14,4' => 'iMac 21" (2014)',
+        'iMac15,1' => 'Retina iMac 27" (2014)',
+
+        'MacBookAir6,1' => 'MBA 11" (2013-14)',
+        'MacBookAir6,2' => 'MBA 13" (2013-14)',
+        'MacBookAir7,1' => 'MBA 11" (2015)',
+        'MacBookAir7,2' => 'MBA 13" (2015)',
+
+        'MacBookPro6,1' => 'MBP 17" (2010)',
+        'MacBookPro6,2' => 'MBP 15" (2010)',
+        'MacBookPro7,1' => 'MBP 13" (2010)',
+        'MacBookPro8,1' => 'MBP 13" (2011)',
+        'MacBookPro8,2' => 'MBP 15" (2011)',
+        'MacBookPro8,3' => 'MBP 17" (2011)',
+        'MacBookPro9,1' => 'MBP 15" (2012)',
+        'MacBookPro9,2' => 'MBP 13" (2012)',
+        'MacBookPro10,1' => 'Retina MBP 15" (2012-13)',
+        'MacBookPro10,2' => 'Retina MBP 13" (2012-13)',
+        'MacBookPro11,1' => 'Retina MBP 13" (2013-14)',
+        'MacBookPro11,2' => 'Retina MBP 15" (2013-14)',
+        'MacBookPro11,3' => 'Retina MBP 15" (2013-14)',
+        'MacBookPro12,1' => 'Retina MBP 13" (2015)',
+      }
     },
     'Number of CPU Cores' => {
       :field => 'ncpu'
