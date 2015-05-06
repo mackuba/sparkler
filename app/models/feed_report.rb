97bf07e634de9350d4aaee79c05762c339a36e04
Kuba Suder <jakub.suder@gmail.com>
2015-05-07 00:22:56 +0200

    FeedReport refactoring

--- app/models/feed_report.rb
+++ app/models/feed_report.rb
@@ -7,32 +7,36 @@ class FeedReport
   cattr_accessor :report_types
 
   def initialize(feed, options = {})
-    @feed = feed
     @include_counts = options[:include_counts]
-    @properties = Property.all.includes(:options)
     @report_types = options[:report_types] || self.class.report_types
 
+    @feed = feed
     @months = feed.statistics.select("DISTINCT #{YM_FORMAT} AS ym").order('ym').map(&:ym)
+    @properties = Property.all.includes(:options)
 
-    calculate_stats
+    calculate_counts
+    calculate_sums
     generate_reports
   end
 
-  def calculate_stats
-    @stats = {}
+  def calculate_counts
+    @counts = {}
 
     grouped_statistics = @feed.statistics
       .select("#{YM_FORMAT} AS ym, property_id, option_id, SUM(counter) AS total_for_month")
       .group("ym, property_id, option_id")
 
     grouped_statistics.each do |stat|
-      property_stats = @stats[stat.property_id] ||= {}
+      property_stats = @counts[stat.property_id] ||= {}
       option_stats = property_stats[stat.option_id] ||= {}
       option_stats[stat.ym] = stat.total_for_month
     end
+  end
 
+  def calculate_sums
     @sums = {}
-    @stats.each do |property_id, property_stats|
+
+    @counts.each do |property_id, property_stats|
       property_sums = @sums[property_id] ||= {}
       property_stats.each do |option_id, option_stats|
         option_stats.each do |ym, count|
@@ -43,14 +47,14 @@ class FeedReport
     end
   end
 
-  def sum_for(property_id, ym)
-    @sums[property_id] && @sums[property_id][ym] || 0
-  end
-
   def count_for(property_id, option_id, ym)
-    property_stats = @stats[property_id] || {}
+    property_stats = @counts[property_id] || {}
     option_stats = property_stats[option_id] || {}
     option_stats[ym] || 0
+  end
+
+  def sum_for(property_id, ym)
+    @sums[property_id] && @sums[property_id][ym] || 0
   end
 
   def generate_reports
@@ -61,92 +65,103 @@ class FeedReport
 
       property = @properties.detect { |p| p.name == options[:field] } || Property.create!(name: options[:field])
 
-      option_converter = case options[:options]
+      converting_proc = case options[:options]
         when Proc then options[:options]
         when Hash then lambda { |title| options[:options][title] || title }
         else lambda { |title| title }
       end
 
-      grouping = options[:group_by] || lambda { |title| title }
-      sorting = options[:sort_by] || lambda { |title| [title.to_i, title.downcase] }
+      grouping_proc = options[:group_by] || lambda { |title| title }
+      sorting_proc = options[:sort_by] || lambda { |title| [title.to_i, title.downcase] }
 
-      option_ids_for_title = {}
+      option_map = processed_options(property.options, grouping_proc, converting_proc, sorting_proc)
 
-      property.options.each do |option|
-        title = option_converter.call(grouping.call(option.name)).to_s
-        option_ids_for_title[title] ||= []
-        option_ids_for_title[title] << option.id
-      end
-
-      data_lines = option_ids_for_title.keys.sort_by(&sorting).map do |title|
-        option_ids = option_ids_for_title[title]
-
-        amounts = []
-        normalized_amounts = []
-
-        @months.each do |ym|
-          amount = option_ids.sum { |option_id| count_for(property.id, option_id, ym) }
-          total = sum_for(property.id, ym)
-          normalized = (total > 0) ? (amount * 100.0 / total).round(1) : 0
-
-          amounts.push(amount)
-          normalized_amounts.push(normalized)
+      data_lines = option_map.map do |title, options|
+        amounts, normalized = calculate_dataset(property) do |ym, i|
+          options.sum { |o| count_for(property.id, o.id, ym) }
         end
 
-        { title: title, amounts: amounts, normalized: normalized_amounts }
+        { title: title, amounts: amounts, normalized: normalized }
       end
 
       data_lines.delete_if { |d| d[:amounts].sum == 0 }
 
       if options[:threshold]
-        other = []
-
-        data_lines.clone.each do |dataset|
-          if dataset[:normalized].max < options[:threshold]
-            data_lines.delete(dataset)
-            other.push(dataset)
-          end
-        end
-
-        amounts = []
-        normalized_amounts = []
-
-        @months.each_with_index do |ym, i|
-          amount = other.sum { |dataset| dataset[:amounts][i] }
-          total = sum_for(property.id, ym)
-          normalized = (total > 0) ? (amount * 100.0 / total).round(1) : 0
-
-          amounts.push(amount)
-          normalized_amounts.push(normalized)
-        end
-
-        other_dataset = {
-          title: "Other",
-          is_other: true,
-          amounts: amounts,
-          normalized: normalized_amounts
-        }
-      
-        data_lines.push(other_dataset)
+        data_lines = extract_other_dataset(data_lines, property, options[:threshold])
       end
 
       if options[:only_counts]
         data_lines.each { |dataset| dataset.delete(:normalized) }
-      elsif !@include_counts
-        data_lines.each { |dataset| dataset.delete(:amounts) }
+      else
+        if !@include_counts
+          data_lines.each { |dataset| dataset.delete(:amounts) }
+        end
       end
 
-      report = { title: report_title, months: @months, series: data_lines }
+      report = {
+        title: report_title,
+        months: @months,
+        series: data_lines,
+        initial_range: case @months.length
+          when 1 then 'month'
+          when 2..12 then 'year'
+          else 'all'
+        end
+      }
 
-      report[:is_downloads] = true if options[:is_downloads]
-      report[:show_other] = false if options[:show_other] == false
-      report[:initial_range] = case @months.length
-        when 1 then 'month'
-        when 2..12 then 'year'
-        else 'all'
-      end
+      report[:is_downloads] = options[:is_downloads] if options.has_key?(:is_downloads)
+      report[:show_other] = options[:show_other] if options.has_key?(:show_other)
 
       @reports.push(report)
     end
+  end
+
+  def processed_options(options, grouping_proc, converting_proc, sorting_proc)
+    grouped_options = {}
+
+    options.each do |option|
+      title = converting_proc.call(grouping_proc.call(option.name).to_s).to_s
+      grouped_options[title] ||= []
+      grouped_options[title] << option
+    end
+
+    sorted_options = {}
+
+    grouped_options.keys.sort_by(&sorting_proc).each do |title|
+      sorted_options[title] = grouped_options[title]
+    end
+    
+    sorted_options
+  end
+
+  def calculate_dataset(property, &amount_proc)
+    amounts = []
+    normalized_amounts = []
+
+    @months.each_with_index do |ym, index|
+      amount = amount_proc.call(ym, index)
+      total = sum_for(property.id, ym)
+      normalized = (total > 0) ? (amount * 100.0 / total).round(1) : 0
+
+      amounts.push(amount)
+      normalized_amounts.push(normalized)
+    end
+
+    [amounts, normalized_amounts]
+  end
+
+  def extract_other_dataset(data_lines, property, threshold)
+    other = data_lines.select { |dataset| dataset[:normalized].max < threshold }
+
+    amounts, normalized = calculate_dataset(property) { |ym, i| other.sum { |dataset| dataset[:amounts][i] }}
+
+    other_dataset = {
+      title: 'Other',
+      is_other: true,
+      amounts: amounts,
+      normalized: normalized
+    }
+
+    data_lines - other + [other_dataset]
   end
 end
